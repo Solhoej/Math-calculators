6642f2c0357ff82604adbebbe5921482a1400e2e
KristianS22R <142444308+Solhoej@users.noreply.github.com>
2023-11-22 20:15:58 +0100

    Quadratic formula part doesnt work
    Currently fixing saving this for emergencies

--- sketch.js
+++ sketch.js
@@ -1,68 +1,84 @@
 let xValue, aValue, bValue, rValue;
+let y1, y2;
+let cirkelForskrift = '(2-1)^2+(y-2)^2=4^2';
+let inputChanged = false;
 
 function setup() {
   createCanvas(400, 400);
-  stringInp = createInput('');
+  stringInp = createInput(cirkelForskrift);
   stringInp.attribute('placeholder', 'Skriv forskrift og definer x: (x-a)^2+(y-b)^2=r^2');
   stringInp.size(310);
-  stringInp.position(40,10);
-  stringInp.input(circleFunction);
+  stringInp.position(40, 10);
 }
 
 function draw() {
   background(220);
   strokeWeight(2);
   rect(35, 6, 330, 30, 2);
+
+  if (keyIsPressed) {
+    inputChanged = true;
+  }
+
+  if (inputChanged) {
+    calculateY();
+    console.log("y1 er " + y1);
+    console.log("y2 er " + y2);
+    inputChanged = false;
+  }
 }
 
-function circleFunction()
-{
+function calculateY() {
   cirkelForskrift = stringInp.value();
 
   let currentNumber = "";
 
-  for (let i = 0; i <cirkelForskrift.length; i++)
-  {
+  for (let i = 0; i < cirkelForskrift.length; i++) {
     let currentChar = cirkelForskrift.charAt(i);
 
-    if (/\d/.test(currentChar) && cirkelForskrift.charAt(i-1) !== '^')
-    {
+    if (/\d/.test(currentChar) && cirkelForskrift.charAt(i - 1) !== '^') {
       currentNumber += currentChar;
     } else {
-      if (currentNumber !== "")
-      {
+      if (currentNumber !== "") {
         numCount = 1;
-        switch (numCount)
-        {
+        switch (numCount) {
           case 1:
             xValue = parseInt(currentNumber);
-            numCount++
+            console.log(xValue);
+            numCount++;
             break;
           case 2:
             aValue = parseInt(currentNumber);
-            numCount++
+            console.log(aValue);
+            numCount++;
             break;
           case 3:
             bValue = parseInt(currentNumber);
-            numCount++
+            console.log(bValue);
+            numCount++;
             break;
           case 4:
             rValue = parseInt(currentNumber);
-            numCount = 1
+            numCount++;
+            console.log(rValue);
             break;
+            default:
+              numCount = 1;
+              break;
         }
         currentNumber = "";
       }
     }
   }
-  console.log(xValue);
-  console.log(aValue);
-  console.log(bValue);
-  console.log(rValue);
-}
 
-function calculateY()
-{
-  y1 = (-(2*bValue)+sqrt(sq(2*bValue)-4*(sq(bValue)+sq(xValue)+sq(aValue)-2*aValue*xValue+sq(rValue))))/2
-  y1 = (-(2*bValue)-sqrt(sq(2*bValue)-4*(sq(bValue)+sq(xValue)+sq(aValue)-2*aValue*xValue+sq(rValue))))/2
+  let discriminant = (sq(2 * bValue)) - (4 * (sq(bValue) + sq(xValue) + sq(aValue) - 2 * aValue * xValue + sq(rValue)));
+
+  if (discriminant >= 0) {
+    y1 = (-(2 * bValue) + sqrt(discriminant)) / 2;
+    y2 = (-(2 * bValue) - sqrt(discriminant)) / 2;
+    console.log("y1:" + y1);
+    console.log("y2:" + y2);
+  } else {
+    console.log("No real solutions for y");
+  }
 }
